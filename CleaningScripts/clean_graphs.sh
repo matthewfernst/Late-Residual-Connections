429054798c8b6bf56829d5fd95ccf3adc80ebab5
matthewfernst <matthew.f.ernst@gmail.com>
2022-04-15 17:24:24 -0600

    update scripts to only be run in correct directory

--- CleaningScripts/clean_graphs.sh
+++ CleaningScripts/clean_graphs.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
-cd ../Graphs
-rm -rf *
+CURRENT_DIRECTORY=$(basename $PWD)
+
+if [ "${CURRENT_DIRECTORY}" == "CleaningScripts" ]; then
+    cd ../Graphs
+    echo "Cleaning graphs..."
+    rm -rf *
+    echo "Done."
+else
+    echo "Please execute script in CleaningScripts directory."
+    exit 1
+fi
--- CleaningScripts/clean_results.sh
+++ CleaningScripts/clean_results.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-cd ../Code/Results
-rm -rf *
+CURRENT_DIRECTORY=$(basename $PWD)
+
+if [ "${CURRENT_DIRECTORY}" == "CleaningScripts" ]; then
+    cd ../Code/Results
+    echo "Cleaning results..."
+    rm -rf *
+    echo "Done."
+else
+    echo "Please execute script in CleaningScripts directory."
+fi
